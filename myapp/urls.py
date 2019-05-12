7530dae6654908e52e42cc40a45889206aa019bf
jalajkhanna <jalaj.khanna@gmail.com>
2019-05-12 16:04:21 -0400

    added base pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about',views.about, name='about'),
+    path('purchase', views.purchase, name='purchase'),
+    path('myaccount', views.myaccount, name='myaccount')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def myaccount(request):
+    return render(request, 'myaccount.html')
+def purchase(request):
+    return render(request, 'purchase.html')
