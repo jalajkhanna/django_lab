4b44dc492a057b4c1b08af58127d92441283c4d6
Shobhu20 <shobhit.2094@gmail.com>
2019-06-22 00:17:51 -0400

    Added action to update stock by 50 items

--- myapp/admin.py
+++ myapp/admin.py
@@ -8,8 +8,16 @@ admin.site.register(Client)
 admin.site.register(Order)
 
 
+def add_50_items(modeladmin, request, queryset):
+    for product in queryset:
+        product.stock+= 50
+        product.save()
+add_50_items.short_description = 'Add 50 items to stock'
+
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'available')
+    actions = [add_50_items,]
 admin.site.register(Product, ProductAdmin)
 
 
