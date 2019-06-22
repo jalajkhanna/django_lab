17f0230684d4c405d1bb64207e674bbfc6f812b2
Shobhu20 <shobhit.2094@gmail.com>
2019-06-22 00:06:48 -0400

    Created Product Admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Product, Category, Client, Order
 
 # Register your models here.
-admin.site.register(Product)
+
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(Order)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+admin.site.register(Product, ProductAdmin)
+
+
 
 
