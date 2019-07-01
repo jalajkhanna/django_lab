772d55e3d48f07f0fd198aa35997e791c6b4a92e
Shobhu20 <shobhit.2094@gmail.com>
2019-07-01 06:50:01 -0400

    Forgot password html changed, base.html changed

--- MySiteS19/urls.py
+++ MySiteS19/urls.py
@@ -16,9 +16,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'myapp/', include('myapp.urls',namespace='myapp')),
     path('admin/', admin.site.urls),
-     url('^', include('django.contrib.auth.urls'))
+     url('^', include('django.contrib.auth.urls')),
+path('password-reset',
+            auth_views.PasswordResetView.as_view(
+            template_name='myapp/password_reset_form.html',
+            email_template_name='myapp/password_reset_email.html',
+            subject_template_name="myapp/password_reset_subject.txt",
+            ),
+             name="password_reset"),
+
+    path('password-reset/done',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='myapp/password_reset_done.html'),
+             name="password_reset_done"),
+
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='myapp/password_reset_complete.html'),
+             name="password_reset_complete"),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='myapp/password_reset_confirm.html'),
+             name="password_reset_confirm"),
 ]
